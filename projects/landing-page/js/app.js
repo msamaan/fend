2c403e6f6c7a9cd1074f45454f6c8d94f6ac46e4
Mirna Samaan <43961305+mirnasamaan2@users.noreply.github.com>
2021-10-16 11:33:00 +0200

    navigation menu

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -39,7 +39,30 @@
 */
 
 // build the nav
+const navElement = document.querySelector('#navbar__list');
+// navElement.style.cssText = 'position: fixed; width: 100%; margin-top: 20px;';
+const sectionOneAnchor = "<li class='nav-link'><a href='#section1'>Section 1</a></li>";
+const sectionTwoAnchor = "<li class='nav-link'><a href='#section2'>Section 2</a></li>";
+const sectionThreeAnchor = "<li class='nav-link'><a href='#section3'>Section 3</a></li>";
+navElement.insertAdjacentHTML('afterbegin', sectionThreeAnchor);
+navElement.insertAdjacentHTML('afterbegin', sectionTwoAnchor);
+navElement.insertAdjacentHTML('afterbegin', sectionOneAnchor);
 
+//Not using forEach for compatability with different browsers
+const navLinks = document.querySelectorAll('.nav-link');
+for(let i = 0; i < navLinks.length; i++) {
+    let elem = navLinks[i];
+    elem.style.padding = '10px';
+    elem.firstChild.style.cssText = 'color: #000; text-decoration: none; padding: 10px;';
+    elem.addEventListener('mouseenter', function() {
+        elem.style.backgroundColor = '#000';
+        elem.firstChild.style.color = '#fff';
+    })
+    elem.addEventListener('mouseleave', function() {
+        elem.style.backgroundColor = '#fff';
+        elem.firstChild.style.color = '#000';
+    })
+}
 
 // Add class 'active' to section when near top of viewport
 
